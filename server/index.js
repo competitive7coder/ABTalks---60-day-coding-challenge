14d47bb1017d3e852c1216f1213b9df92048312d
Protyush <ghorui.protyushraj@gmail.com>
2026-08-08 15:54:04 +0530

    feat: expose leaderboard route gateway

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import challengeRoute from './routes/challenge.route.js'
 import submissionRoute from './routes/submission.route.js'
 
 import auth from "./middleware/auth.js"
-import { userLoginController, userLogOutController, userRefressingTokenController, userRegisterController } from './controller/user.controller.js'
+import { userLoginController, userLogOutController, userRefressingTokenController, userRegisterController, getCurrentUserController, getLeaderboardController } from './controller/user.controller.js'
 
 
 const app = express()
@@ -43,6 +43,8 @@ app.post("/sign-up", userRegisterController)
 app.post("/sign-in", userLoginController)
 app.post("/logout", userLogOutController)
 app.get("/refreshToken", userRefressingTokenController)
+app.get("/me", auth, getCurrentUserController)
+app.get("/leaderboard", auth, getLeaderboardController)
 
 // Route level middlewire
 app.use("/api/challenge", challengeRoute)
