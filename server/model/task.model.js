7f2103a913ad02aa32adc2ec4657e57d5d676680
Protyush <ghorui.protyushraj@gmail.com>
2026-08-08 19:52:15 +0530

    Add task.model.js

---
+++ server/model/task.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const resourceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["video", "article", "documentation", "other"],
+      default: "article",
+    },
+    duration: {
+      type: String,
+      default: "",
+    },
+  },
+  { _id: false }
+);
+
+const taskSchema = new mongoose.Schema(
+  {
+    day: {
+      type: Number,
+      required: true,
+    },
+    task: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    difficulty_level: {
+      type: String,
+      enum: ["Easy", "Medium", "Hard"],
+      default: "Medium",
+    },
+    requirements: {
+      type: [String],
+      default: [],
+    },
+    acceptanceCriteria: {
+      type: [String],
+      default: [],
+    },
+    resources: {
+      type: [resourceSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const taskModel = mongoose.model("Task", taskSchema);
+
+export default taskModel;
