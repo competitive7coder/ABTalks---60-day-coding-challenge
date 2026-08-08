b922101a6af64f4c21f45b0f4b35efe77e1c4145
Protyush <ghorui.protyushraj@gmail.com>
2026-08-08 15:54:09 +0530

    refactor: configure challenge routes

--- server/routes/challenge.route.js
+++ server/routes/challenge.route.js
@@ -2,11 +2,14 @@ import express from 'express'
 
 import auth from '../middleware/auth.js'
 import { createChallenge, getChallengeByDay, getCurrentChallenge } from '../controller/challenge.controller.js'
+import { submitChallenge, getUserSubmissions } from '../controller/submission.controller.js'
 
 const challengeRoute = express.Router()
 
 challengeRoute.post("/create", auth, createChallenge)
 challengeRoute.get("/current", auth, getCurrentChallenge)
+challengeRoute.post("/submit", auth, submitChallenge)
+challengeRoute.get("/submissions/all", auth, getUserSubmissions)
 challengeRoute.get("/:day", auth, getChallengeByDay)
 
 export default challengeRoute
